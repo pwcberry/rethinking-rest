bd94b37f64cefeb7f614cb34b207ac49f8d7a629
Peter Berry <peter.berry@rea-group.com>
2019-08-20 11:48:40 +1000

    WIP

--- client/project/index.js
+++ client/project/index.js
@@ -15,7 +15,24 @@ fragment commitFragment on Repository {
 }
 `;
 
-let queryRepoList;
+let queryRepoList = `
+{
+  viewer {
+    name
+    repos:repositories(first:20, orderBy:{ field: UPDATED_AT, direction:DESC }) {
+      totalCount
+      nodes {
+        name
+        url
+        updatedAt
+        ...commitFragment
+      }
+    }
+  }
+}
+
+${commitFragment}
+`;
 
 let mutationAddStar;
 
@@ -23,7 +40,27 @@ let mutationRemoveStar;
 
 function gqlRequest(query, variables, onSuccess) {
   // MAKE GRAPHQL REQUEST
-
+  $.post({
+    url: "https://api.github.com/graphql",
+    contentType: "application/json",
+    headers: {
+      Authorization: "bearer 57108de1e9fddd8e3204c15d6350d649aa6a4673",
+      // Authorization: "token 27dc55fca8ddf1fbb71c819fa43d72145398825a"
+    },
+    data: JSON.stringify({
+      query,
+      variables,
+    }),
+    success: (response) => {
+      const {
+        data
+      } = response;
+      if (data) {
+        onSuccess(data);
+      }
+    },
+    error: (error) => console.error(error)
+  });
 }
 
 function starHandler(element) {
@@ -31,7 +68,23 @@ function starHandler(element) {
 
 }
 
-$(window).ready(function() {
+$(window).ready(function () {
   // GET NAME AND REPOSITORIES FOR VIEWER
+  gqlRequest(queryRepoList, {}, (data) => {
+    $("header h2").text(data.viewer.name);
+    const {
+      repos
+    } = data.viewer;
 
+    if (repos.totalCount > 0) {
+      const container = $(".repos");
+      container.empty();
+      repos.nodes.forEach(node => {
+        container.append(`<article class="repo">
+          <h3>${node.name}</h3>
+          <p>${node.ref.target.history.totalCount} commits</p>
+        </li>`);
+      });
+    }
+  })
 });
